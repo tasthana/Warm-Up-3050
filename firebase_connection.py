8e203a412648dd0555850c89b6e35a5e1ae16ec4
nathanblanchard <njblanch@uvm.edu>
2024-02-12 16:42:07 -0500

    Added some functions to firebase_connection.py

--- firebase_connection.py
+++ firebase_connection.py
@@ -7,15 +7,29 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+
+
 #credentials and verification 
-cred = credentials.Certificate('warm-up-project-3050.json')
-app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+def verify_connection(cert_name):
+    #cred = credentials.Certificate('warm-up-project-3050.json')
+    cred = credentials.Certificate(cert_name)
+    app = firebase_admin.initialize_app(cred)
+    client = firestore.client()
+    return client
 
 #selecting the database collection
-users_ref = db.collection("3050-Dealership")
-docs = users_ref.stream()
+def retrieve_reference(collection_name, client):
+    #ref = db.collection("3050-Dealership")
+    ref = client.collection(collection_name)
+    return ref
 
-#printing it all out.= 
-for doc in docs:
-    print(f"{doc.id} => {doc.to_dict()}")
+#printing it all out
+def print_collection(ref):
+    docs = ref.stream()
+    for doc in docs:
+        print(f"{doc.id} => {doc.to_dict()}")
+
+
+client = verify_connection('warm-up-project-3050.json')
+ref = retrieve_reference("3050-Dealership", client)
+print_collection(ref)

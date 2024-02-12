4c63191e2b9e8ef5023f637d5a43f9e5cbbd6884
nathanblanchard <njblanch@uvm.edu>
2024-02-12 16:42:43 -0500

    Made some changes with uuid and to_dict

--- car.py
+++ car.py
@@ -1,5 +1,6 @@
 class Car:
-    def __init__(self, make, model, color, msrp, quantity, mpg=-1, horsepower=-1):
+    def __init__(self, uuid, make, model, color, msrp, quantity, mpg=-1, horsepower=-1):
+        self.uuid = uuid
         self.make = make
         self.model = model
         self.color = color
@@ -10,21 +11,20 @@ class Car:
 
     @staticmethod
     def from_dict(source):
-       return Car(source['make'], source['model'], source['color'], source['msrp'], source['quantity'],
+       return Car(source['uuid'], source['make'], source['model'], source['color'], source['msrp'], source['quantity'],
                    source.get('mpg', -1), source.get('horsepower', -1))
 
     def to_dict(self):
       car_dict = {
+            'uuid': self.uuid,
             'make': self.make,
             'model': self.model,
             'color': self.color,
             'msrp': self.msrp,
-            'quantity': self.quantity
+            'quantity': self.quantity,
+            'mpg': self.mpg,
+            'horsepower': self.horsepower
         }
-      if self.mpg != -1:
-          car_dict['mpg'] = self.mpg
-      if self.horsepower != -1:
-          car_dict['horsepower'] = self.horsepower
       return car_dict
        
     def __repr__(self):
